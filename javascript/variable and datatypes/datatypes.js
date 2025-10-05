89907a9f60e544bed51f1ab3066dca581d2d0559
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-05 22:14:17 +0530

    Refactor datatypes.js to include console logs for all data types

--- javascript/variable and datatypes/datatypes.js
+++ javascript/variable and datatypes/datatypes.js
@@ -5,28 +5,35 @@
 
 //Number
 const  num = 100  // this is a example of number data type
+console.log(typeof num)
 
 
 // string 
 const name  = "this is a string"   // this is string
+console.log(typeof name)
+
 
 // boolean
 let login  = true    // this is a boolean data type its contain true or false
+console.log(typeof login)
 
 
 //undefine
 let user;     // this is a undefine data type, if you declare a variable but not assign any value then by default its assign undefine, because you did't assign any value.
 
+console.log(typeof user);
+
 
 //bigint
 let n = 4187446764174687654n 
+console.log(typeof n);
 
 // Null
 let temp = null
-
+console.log(typeof temp)
 //Symbol 
 const id1 = Symbol("id") // for creating unique value
-
+console.log(typeof id1)
 
 
 
@@ -37,14 +44,14 @@ const id1 = Symbol("id") // for creating unique value
 
 // Array
 arr = [1,2,3,4,5,6,7,8,9,0 ]
-
+console.log(typeof arr)
 
 // Object 
 let obj = {
     name:"ashamin",
     id: "123456"
 }
-
+console.log(typeof obj)
 
 // function
 
@@ -53,3 +60,4 @@ function add (){
 }
  add()
 
+console.log(typeof add)
