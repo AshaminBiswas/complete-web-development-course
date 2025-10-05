058c2cf110220fc4d406a36367b1f1b7a31c7ee1
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-05 21:59:52 +0530

    Primitive data types

---
+++ javascript/variable and datatypes/datatypes.js
@@ -0,0 +1,27 @@
+// Data Type In javascript
+
+// there is two types of data exists in javascript
+// Primitive and non Primitive
+
+//Number
+const  num = 100  // this is a example of number data type
+
+
+// string 
+const name  = "this is a string"   // this is string
+
+// boolean
+let login  = true    // this is a boolean data type its contain true or false
+
+
+//undefine
+let user;     // this is a undefine data type, if you declare a variable but not assign any value then by default its assign undefine, because you did't assign any value.
+
+
+//bigint
+let n = 4187446764174687654n 
+
+// Null
+let temp = null
+
+
