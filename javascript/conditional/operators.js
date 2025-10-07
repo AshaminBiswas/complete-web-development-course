f19fdb1f136e71d359c4b03bf30354337984de91
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-07 20:06:20 +0530

    Add operators.js with arithmetic and loop examples

---
+++ javascript/conditional/operators.js
@@ -0,0 +1,26 @@
+// Arithmetic operator
+
+let a = 20
+let b = 30
+console.log(a + b);
+console.log(a < b);
+console.log(a > b);
+console.log(a == b);
+
+
+
+
+
+//Loop
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+    
+}
+
+let h = 1;
+while(h < 200){
+    console.log(h);
+    h++
+    
+}
