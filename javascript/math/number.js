676c4b29bfb2c4d34ede0fad41e39740c6774f55
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-08 21:14:43 +0530

    Comment out example code in number.js for clarity

--- javascript/math/number.js
+++ javascript/math/number.js
@@ -1,10 +1,13 @@
-let a = 23.3654
+// let a = 23.3654
 
-console.log(a.toFixed(2));
-console.log(a.toString());
+// console.log(a.toFixed(2));
+// console.log(a.toString());
 
 
-let b = new Number(20)
-console.log(b);
+// let b = new Number(20)
+// console.log(b);
 
-console.log(Math.floor(Math.random()* 10));
+// console.log(Math.floor(Math.random()* 10));
+
+
+console.log(Math.floor(Math.random()*7)+1);
