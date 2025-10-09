b8f297d666da1908c181a9d2a46df14e38c9efa6
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-09 19:41:52 +0530

    Comment out example code in string.js for clarity

--- javascript/string/string.js
+++ javascript/string/string.js
@@ -1,16 +1,23 @@
 let a = " string one";
 let b = "string two";
-let c = ` string three`;
+let c = ` string three `;
 
-console.log(a, b, c);
+// console.log(a, b, c);
 
-console.log(a.length);
-console.log(a[2]);
-console.log(a.toUpperCase())
-console.log(a.toLowerCase())
+// console.log(a.length);
+// console.log(a[2]);
+// console.log(a.toUpperCase())
+// console.log(a.toLowerCase())
 
-console.log(a.indexOf("o"));
+// console.log(a.indexOf("o"));
 
-console.log(b.includes("two"))
-console.log(c.slice(1,5));
+// console.log(b.includes("two"))
+// console.log(c.slice(1,5));
 
+// console.log(a.replace("one", "first"));
+
+// console.log(c.trim());
+
+let friends = "ashamin,ushamin,asik,raju,mousin"
+
+console.log(friends.split(","))
