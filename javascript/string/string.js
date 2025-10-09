518f75d550238662d6f776f5d160be41c65309d1
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-09 19:30:09 +0530

    Add string.js with examples of string manipulation and methods

---
+++ javascript/string/string.js
@@ -0,0 +1,16 @@
+let a = " string one";
+let b = "string two";
+let c = ` string three`;
+
+console.log(a, b, c);
+
+console.log(a.length);
+console.log(a[2]);
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+
+console.log(a.indexOf("o"));
+
+console.log(b.includes("two"))
+console.log(c.slice(1,5));
+
