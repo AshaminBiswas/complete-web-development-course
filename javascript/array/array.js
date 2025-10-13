d186fd1e6ae69a78fb0ab9c476976fe22761743e
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-13 20:19:36 +0530

    Add array.js with examples of array manipulation and mutability

---
+++ javascript/array/array.js
@@ -0,0 +1,16 @@
+//Array
+
+let marks = [100,80,75,67,95]
+
+// console.log(marks);
+
+// array are mutable, it's mean if can make any changes in a array then its change the original array
+
+let arr = [100, "ashamin", false ]
+arr[1] = "biswas"
+// console.log(arr);
+arr.push("lol")
+console.log(arr);
+arr.pop()
+console.log(arr);
+
