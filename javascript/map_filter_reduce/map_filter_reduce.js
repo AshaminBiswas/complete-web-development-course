27939c0420bc46d8df41c633d30f71c65d27c9df
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-28 21:15:08 +0530

    Add custom filtering method and foreach example to demonstrate array manipulation

--- javascript/map_filter_reduce/map_filter_reduce.js
+++ javascript/map_filter_reduce/map_filter_reduce.js
@@ -1,2 +1,28 @@
 // let arr = [1,2,3,4,5,6]
 // console.log(arr.join());
+
+// foreach
+
+let arr = [1, 2, 3, 4, 5, 6];
+let sum = 0;
+arr.forEach((item) => {
+  sum = sum + item;
+});
+
+// console.log(sum);
+
+// Filter
+let newArray = arr.filter((item) => item < 3);
+// console.log(newArray)
+
+arr.filtering = function (cb) {
+  let ans = [];
+  for (let number of this) {
+    if (cb(number)) {
+      ans.push(number);
+    }
+  }
+  return ans
+};
+
+// console.log(arr.filtering((num)=> num > 2));
