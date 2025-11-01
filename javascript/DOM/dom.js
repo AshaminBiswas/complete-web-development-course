90abc49550733534e4ccebd57d2ea245ff209ca6
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-11-01 20:48:12 +0530

    Add new heading elements and update DOM manipulation for improved structure

--- javascript/DOM/dom.js
+++ javascript/DOM/dom.js
@@ -9,4 +9,12 @@ const heading = document.querySelector(".heading")
 // Create Element
 const newElement = document.createElement("h2")
 newElement.textContent = "This is Heading Two"
+
+const newElement2 = document.createElement("h3")
+// Add Class On tag
+newElement2.className = "diwali"
+newElement2.classList.add("h3")
+newElement2.textContent = "I am H3 "
+
 heading.after(newElement)
+newElement.after(newElement2)
