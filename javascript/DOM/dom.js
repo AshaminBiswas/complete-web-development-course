fa2072bfb3f90754ce218ee85b24180951e5dc9b
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-11-01 20:40:50 +0530

    Refactor HTML structure and update JavaScript to create new heading element

--- javascript/DOM/dom.js
+++ javascript/DOM/dom.js
@@ -1,5 +1,12 @@
-document.getElementById("main")
-document.getElementsByTagName("h1")
-document.getElementsByClassName("heading")
-document.querySelector(".heading")
-document.querySelectorAll("h1")
+// document.getElementById("main")
+// document.getElementsByTagName("h1")
+// document.getElementsByClassName("heading")
+// document.querySelector(".heading")
+// document.querySelectorAll("h1")
+
+const heading = document.querySelector(".heading")
+
+// Create Element
+const newElement = document.createElement("h2")
+newElement.textContent = "This is Heading Two"
+heading.after(newElement)
