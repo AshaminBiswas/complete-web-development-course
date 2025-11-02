c733e556dbdcd05449ae2376c2d6dbe66c1235f2
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-11-02 21:54:21 +0530

    Refactor HTML and JavaScript structure for improved readability and organization

--- javascript/events/event.js
+++ javascript/events/event.js
@@ -2,3 +2,20 @@ const h1 = document.querySelector("h1")
 h1.addEventListener("click",()=>{
     h1.textContent = "Hello From Ashamin"
 })
+
+
+const grandParent = document.querySelector("#grand-parent")
+const parent = document.querySelector("#parent")
+const child = document.querySelector("#child")
+
+grandParent.addEventListener("click",()=>{
+    console.log("Grand Parent is Clicked")
+})
+parent.addEventListener("click",(e)=>{
+    e.stopPropagation()
+    console.log("Parent is Clicked")
+})
+child.addEventListener("click",(e)=>{
+    e.stopPropagation()
+    console.log("Child is Clicked")
+})
