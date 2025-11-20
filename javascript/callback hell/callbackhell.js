c7b0a91936ef6d662294e7bacd4b303b20a3ad2b
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-11-20 22:00:39 +0530

    Add initial HTML and JavaScript files for callback hell demonstration

---
+++ javascript/callback hell/callbackhell.js
@@ -0,0 +1,44 @@
+
+// build a simple zomato app
+
+function placeOrder(cb){
+    console.log("payment in progress..");
+
+    setTimeout(()=>{
+        console.log("Payment is SuccessFull,, Order get placed"); 
+        cb()
+    },3000)
+    
+}
+
+function preparingOrder(cb){
+    console.log("You Order is Food Preparing....");
+    setTimeout(()=>{
+        console.log("Your Order is Prepared"); 
+        cb()
+    },3000)
+}
+
+function pickupOrder(){
+    console.log("Delivery boy Pickup your order");
+     setTimeout(()=>{
+        console.log("I pickup the order"); 
+    },3000)
+}
+
+function orderDeliver(){
+    console.log("Order out for deliver");
+     setTimeout(()=>{
+        console.log("Order is Delivered"); 
+    },3000)
+}
+
+
+
+placeOrder(()=>{
+    preparingOrder(()=>{
+        pickupOrder(()=>{
+            orderDeliver()
+        })
+    })
+})
