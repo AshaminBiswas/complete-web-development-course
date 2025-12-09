256c9aeb5e653f4bb7facd25e659b08fe15342f8
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-12-09 21:53:35 +0530

    feat: refactor Card component and update App to use Card instances

--- react/01_start/src/App.jsx
+++ react/01_start/src/App.jsx
@@ -6,42 +6,13 @@ function App() {
 
   return (
     <>
-     {/* <h1 class="text-blue-600 dark:text-sky-400">hello world</h1> */}
-
-     <div className='flex gap-4'>
-      <div className='w-68  border border-amber-400 rounded-2xl'>
-      <img className='rounded-tl-2xl rounded-tr-2xl' src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=" alt="image" />
-      <div className='px-2'>
-        <h1>Card 1</h1>
-        <p className='text-emerald-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, debitis?</p>
-        <button>Buy Now</button>
-      </div>
-     </div>
-     <div className='w-68  border border-amber-400 rounded-2xl'>
-      <img className='rounded-tl-2xl rounded-tr-2xl' src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=" alt="image" />
-      <div className='px-2'>
-        <h1>Card 1</h1>
-        <p className='text-emerald-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, debitis?</p>
-        <button>Buy Now</button>
-      </div>
-     </div>
-     <div className='w-68  border border-amber-400 rounded-2xl'>
-      <img className='rounded-tl-2xl rounded-tr-2xl' src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=" alt="image" />
-      <div className='px-2'>
-        <h1>Card 1</h1>
-        <p className='text-emerald-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, debitis?</p>
-        <button>Buy Now</button>
-      </div>
-     </div>
-     <div className='w-68  border border-amber-400 rounded-2xl'>
-      <img className='rounded-tl-2xl rounded-tr-2xl' src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE=" alt="image" />
-      <div className='px-2'>
-        <h1>Card 1</h1>
-        <p className='text-emerald-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, debitis?</p>
-        <button>Buy Now</button>
-      </div>
-     </div>
-     </div>
+       <div className='flex gap-4' >
+         <Card 
+         title="Buy Python"
+          buttonText="Join Now"/>
+        <Card title={"Buy Javascript"}/>
+        <Card/>
+       </div>
     </>
   )
 }
--- react/01_start/src/components/Card.jsx
+++ react/01_start/src/components/Card.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+function Card({title="Course", buttonText = "Buy Now"}) {
   return (
-    <div>
-      
+    <div className="w-68  border border-amber-400 rounded-2xl">
+      <img
+        className="rounded-tl-2xl rounded-tr-2xl"
+        src="https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+        alt="image"
+      />
+      <div className="px-2">
+        <h1>{title}</h1>
+        <p className="text-emerald-700">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, debitis?
+        </p>
+        <button>{buttonText}</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
