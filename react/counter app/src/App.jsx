b521cf843dc76c618947b4030ceb22b5c59d7d7b
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-12-10 21:57:49 +0530

    feat: add initial layout and functionality for Counter App

--- react/counter app/src/App.jsx
+++ react/counter app/src/App.jsx
@@ -4,6 +4,33 @@ function App() {
   return (
     <div style={{}}>
         <h1>Counter App</h1>
+        <div className='card'>
+            count is 10
+        </div>
+
+        <div style={{gap:"20px", display:"flex"}}>
+          <button
+          onClick={()=>{}}
+          >Increase</button>
+          <button
+          onClick={()=>{}}
+          >Decrease</button>
+          <button
+          onClick={()=>{}}
+          >Reset</button>
+        </div>
+
+        <div 
+        style={{gap:"20px", display:"flex", margin:"20px"}}>
+          <input 
+          type="text" 
+          style={{border:"1px solid white", padding:"0.6em 1.2em"}} 
+          value="8" 
+          onChange={()=>{}}/>
+          <button
+          onClick={()=>{}}
+          >Set to 8</button>
+        </div>
     </div>
   )
 }
