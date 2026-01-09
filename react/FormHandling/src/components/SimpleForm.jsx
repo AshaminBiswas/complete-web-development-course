a80c88c5792bd0643dfb912966d57ceb474d344f
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-01-09 17:06:06 +0530

    feat: initialize React form handling project with Vite
    - Add package.json with dependencies and scripts for development
    - Include Vite logo SVG in public directory
    - Create App.css for styling the application
    - Implement main App component to render SimpleForm
    - Add React logo SVG in assets directory
    - Create SimpleForm component for handling user input
    - Add global styles in index.css
    - Set up main entry point in main.jsx
    - Configure Vite with vite.config.js

---
+++ react/FormHandling/src/components/SimpleForm.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function SimpleForm() {
+    const [name, setName] = useState("")
+    const handelSubmit = (e)=>{
+        e.preventDefault()
+        console.log("Name is :", name)
+    }
+
+  return (
+    <form onSubmit={handelSubmit}>
+      <label htmlFor="">Name</label>
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
+
+export default SimpleForm
